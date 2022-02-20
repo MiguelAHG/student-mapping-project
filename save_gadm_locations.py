f97011e0c1efd5c9ea883b78cc55c58ba4809436
Miguel Antonio H. Germar <migs.germar@gmail.com>
2022-02-20 13:02:23 +0800

    Convert GADM data to TopoJSON

--- save_gadm_locations.py
+++ save_gadm_locations.py
@@ -3,10 +3,17 @@
 import pandas as pd
 import numpy as np
 import fiona
-import Levenshtein
 import geopandas as gpd
+import json
+from pytopojson import topology
 
-#%% Get data
+#%%
+# Determine the finest layer. 3 is barangay, 2 is city, 1 is province.
+finest_layer = 2
+
+#%%
+# Save table of all GADM locations
+# Get data
 gpkg = "./geo_data/gadm36_PHL.gpkg"
 
 # See list of layers in the GPKG.
@@ -14,9 +21,22 @@ layers = fiona.listlayers(gpkg)
 print("GADM Layers: ", layers)
 
 # Use the finest layer, number 3
-gdf = gpd.read_file(gpkg, layer = "gadm36_PHL_3")
+gdf = gpd.read_file(gpkg, layer = f"gadm36_PHL_{finest_layer}")
 print("\nGADM: ", gdf.columns.tolist())
 
-# Save geodata as CSV
-gdf = gdf.drop("geometry", axis = 1)
-gdf.to_csv("./private/cleaning_outputs/gadm_all_locations.csv")
+#%%
+# Save table of locations without map configuration data as CSV
+gdf.drop("geometry", axis = 1).to_csv("./private/cleaning_outputs/gadm_all_locations.csv")
+
+#%%
+# Convert GADM data to TopoJSON
+
+geo_str = gdf.to_json()
+geo_dct = json.loads(geo_str)
+
+tpg = topology.Topology()
+topojson = tpg({"city_geodata": geo_dct})
+
+f = open("./geo_data/city_topojson.json", "w")
+json.dump(topojson, f, indent = 4)
+f.close()
