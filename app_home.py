022b2174648ee6921c081d6133da6e0d00b97a00
Miguel Antonio H. Germar <migs.germar@gmail.com>
2022-03-15 21:18:02 +0800

    Add advice to home page text

--- app_home.py
+++ app_home.py
@@ -5,4 +5,4 @@ import streamlit as st
 def home_feature():
     st.markdown("""# Home Page
 
-Welcome to the ASHS Student-Hazard App.""")
+Welcome to the ASHS Student-Hazard App. Start by visiting the Hazard Map Layer Creator. When you are done making a layer, go to the Report Generator. You may go back to change the layer if needed. Also remember to save your layer and the report outputs. If you close or refresh this tab, your progress will be lost.""")
