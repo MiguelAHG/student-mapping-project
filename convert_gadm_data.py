e148892a1c5b79e78130f07859048ffcf67d1197
Miguel Antonio H. Germar <migs.germar@gmail.com>
2022-07-11 13:32:20 +0800

    Remove code to convert GADM data to TopoJSON
    This code was needed for an Altair map, but a Plotly map is used now.

--- convert_gadm_data.py
+++ convert_gadm_data.py
@@ -4,8 +4,6 @@ import pandas as pd
 import numpy as np
 import fiona
 import geopandas as gpd
-import json
-from pytopojson import topology
 
 #%%
 # Determine the finest layer. 3 is barangay, 2 is city, 1 is province.
@@ -27,16 +25,3 @@ print("\nGADM: ", gdf.columns.tolist())
 #%%
 # Save table of locations without map configuration data as CSV
 gdf.drop("geometry", axis = 1).to_csv("./private/cleaning_outputs/gadm_all_locations.csv")
-
-#%%
-# Convert GADM data to TopoJSON
-
-geo_str = gdf.to_json()
-geo_dct = json.loads(geo_str)
-
-tpg = topology.Topology()
-topojson = tpg({"city_geodata": geo_dct})
-
-f = open("./geo_data/city_topojson.json", "w")
-json.dump(topojson, f, indent = 4)
-f.close()
